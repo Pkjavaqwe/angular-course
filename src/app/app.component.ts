3bf9595c9392a6a9a907326d794970d85239e9b9
Prashik Kamble <prashikkamble808@gmail.com>
2025-01-22 12:22:13 +0530

    committing initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
 
 @Component({
     selector: 'app-root',
@@ -9,6 +9,6 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
-
+    data = { title: "this is the remote repository" }
 
 }
